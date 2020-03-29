ebfee5e4fb161da0f8f8cd19456d18f09384dec4
Anderson Ismael <55637907+aicoutos@users.noreply.github.com>
2020-03-29 10:39:42 -0300

    migration create_mensagens_table adicionada

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# laraguestbook
+# LaraGuestbook
+
+## Funcionalidades
+1. Mensagens de até 280 caracteres
+2. Botão apagar mensagem (baseado em cookie)
+
+## Instalação
---
+++ database/migrations/2020_03_29_132014_create_mensagens_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMensagensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mensagens', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('mensagens', 280);
+            $table->char('cookieDelete', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mensagens');
+    }
+}
